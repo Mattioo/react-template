f4a64d489d6bb16b6d89617a06d5049372a20206
mkorolow <m.korolow@sputnik.pl>
2020-06-02 16:20:27 +0200

    css for choosed client part 1.)

--- source-code/react-template/ClientApp/src/components/app/index.jsx
+++ source-code/react-template/ClientApp/src/components/app/index.jsx
@@ -1,4 +1,3 @@
-import styles from '../../styles/app/styles.scss';
 import React, { Component } from 'react';
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
@@ -7,21 +6,35 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
 library.add(faCheck);
 dom.watch();
 
-const content = 'Hello world!';
-
 class App extends Component {
-  render() {
-      return (
-          <div>
-              <p className={styles.test1}>{content}</p>
-              <p className={styles.test1}>
-                  <span className={styles.test2}>Lorem ipsum..</span>
-              </p>
-              <p className={styles.test1_test3}>Lorem ipsum..</p>
-              <i className={styles.icon + " fas fa-check"}></i>
-          </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            client: `${window.location.protocol}//${window.location.host}`
+        };   
+    }
+
+    render() {
+        this.setStyles(this.state.client);
+        return (
+            <p>Lorem ipsum..</p>
+        );
+    }
+
+    async setStyles(client) {
+        const response = await fetch(`https://localhost:44394/api/app/styles?client=${client}`);
+        if (response.ok) {
+            const stylesName = await response.text();
+
+            var sheet = document.createElement('link');
+            sheet.rel = 'stylesheet';
+            sheet.type = 'text/css';
+            sheet.href = `/clients/${stylesName}/styles.css`;
+
+            document.head.appendChild(sheet);
+        }
+    }
 }
 
 export default App;
