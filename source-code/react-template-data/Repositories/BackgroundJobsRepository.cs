69335e216f4b7b1d190cb2014e275d534e21bb9e
mkorolow <m.korolow@sputnik.pl>
2020-06-21 23:53:51 +0200

    add BackgroundJobsRepository

---
+++ source-code/react-template-data/Repositories/BackgroundJobsRepository.cs
@@ -0,0 +1,11 @@
+﻿using react_template_data.Data;
+using react_template_data.Data.Master;
+
+namespace react_template_data.Repositories
+{
+    public class BackgroundJobsRepository : BaseRepository<BackgroundJob>
+    {
+        public BackgroundJobsRepository(MasterContext context) : base(context)
+        { }
+    }
+}
