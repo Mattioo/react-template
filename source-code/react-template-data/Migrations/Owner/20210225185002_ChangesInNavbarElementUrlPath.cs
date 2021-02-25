99ece6f8277f62507e66d015975eba6748b701e5
mattioo94 <mateuszkorolow@gmail.com>
2021-02-25 19:51:08 +0100

    Add migration to changes in navbar urls

---
+++ source-code/react-template-data/Migrations/Owner/20210225185002_ChangesInNavbarElementUrlPath.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace react_template_data.Migrations.Owner
+{
+    public partial class ChangesInNavbarElementUrlPath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "NavbarElements",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Url",
+                value: "/");
+
+            migrationBuilder.UpdateData(
+                table: "NavbarElements",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Url",
+                value: "/info");
+
+            migrationBuilder.UpdateData(
+                table: "NavbarElements",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Url",
+                value: "/contact");
+
+            migrationBuilder.UpdateData(
+                table: "Pdfs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Default",
+                value: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "NavbarElements",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Url",
+                value: "#");
+
+            migrationBuilder.UpdateData(
+                table: "NavbarElements",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Url",
+                value: "#info");
+
+            migrationBuilder.UpdateData(
+                table: "NavbarElements",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Url",
+                value: "#contact");
+
+            migrationBuilder.UpdateData(
+                table: "Pdfs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Default",
+                value: true);
+        }
+    }
+}
